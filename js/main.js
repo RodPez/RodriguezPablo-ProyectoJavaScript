8ada3b488a80c81f7520136bc7a93c3aa17709c3
RodriguezPablo <pxblox@gmail.com>
2022-11-07 15:49:49 -0300

    Se agrego estilo, se aplico funciones, array y objetos en el JS

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
-const dolar = 153.83;
-const euro = 151.64;
-const real = 29.80;
+class Moneda{
+    constructor(nombre,compra,venta){
+        this.nombre=nombre;
+        this.compra=compra;
+        this.venta=venta;
+    }
 
-let cotizacionDolar = "La cotización del dolar al día de la fecha es" + " " + dolar;
-let cotizacionEuro = "La cotización del euro al día de la fecha es" +" " + euro;
-let cotizacionReal = "La cotizacióin del real al día de la fecha es" + " " + real;
+}
+
+const divisas=[];
+
+const dolar= new Moneda("dolar",153.83,156) ;
+
+const euro= new Moneda("euro",151.64,153);
+
+const real= new Moneda("real",29.80,31 );
+
+divisas.push(dolar,euro,real);
+
+let cotizacionDolar = "La cotización del dolar al día de la fecha es" + " " + dolar.venta +" " +"para la venta" +" "+ "y"+" "+dolar.compra+" "+"para la compra";
+let cotizacionEuro = "La cotización del euro al día de la fecha es" +" " +  euro.venta +" " +"para la venta" +" "+ "y"+" "+euro.compra+" "+"para la compra";
+let cotizacionReal = "La cotizacióin del real al día de la fecha es" + " " +  real.venta +" " +"para la venta" +" "+ "y"+" "+real.compra+" "+"para la compra";
 
 console.log(cotizacionDolar);
 console.log(cotizacionEuro);
@@ -23,32 +38,43 @@ let divisaACambiar = prompt("Indique la divisa con la que quiere opererar")
 
 let montoACambiar = parseFloat(prompt("Indique el monto con el cual desea operar"))
 
-let divisaAPesos = (a,b) => a * b;
-
-let pesosADivisa = (a,b) => a / b;
-
-if (transaccion == "venta" && divisaACambiar == "euro") {
-    divisaAPesos = divisaAPesos(euro , montoACambiar)
-    divisaAPesos = parseInt(divisaAPesos)
-    console.log("Usted recibirá" + " " + divisaAPesos + " " + "pesos Argentinos.");
-}else if (transaccion == "venta" && divisaACambiar == "dolar") {
-    divisaAPesos = divisaAPesos(dolar , montoACambiar)
-    divisaAPesos = parseInt(divisaAPesos)
-    console.log("Usted recibirá" + " " + divisaAPesos + " " + "pesos Argentinos.");
-}else if (transaccion == "venta" && divisaACambiar == "real") {
-    divisaAPesos = divisaAPesos(real , montoACambiar)
-    divisaAPesos = parseInt(divisaAPesos)
-    console.log("Usted recibirá" + " " + divisaAPesos + " " + "pesos Argentinos.");
-}else if (transaccion == "compra" && divisaACambiar == "euro") {
-    divisaAPesos = divisaAPesos(euro , montoACambiar)
-    divisaAPesos = parseInt(divisaAPesos)
-    console.log("El costo de su operación es de" + " " + divisaAPesos + " " + "pesos Argentinos.");
-}else if (transaccion == "compra" && divisaACambiar == "dolar") {
-    divisaAPesos = divisaAPesos(dolar , montoACambiar)
-    divisaAPesos = parseInt(divisaAPesos)
-    console.log("El costo de su operación es de" + " " + divisaAPesos + " " + "pesos Argentinos.");
-}else {
-    divisaAPesos = divisaAPesos(real , montoACambiar)
-    divisaAPesos = parseInt(divisaAPesos)
-    console.log("El costo de su operación es de" + " " + divisaAPesos + " " + "pesos Argentinos.");
+function operacion (valor,monto) {
+    return valor * monto;
 }
+
+let comprar;
+let vender;
+
+function compraDeMoneda(compraMoneda, monto1) {
+    comprar= operacion(compraMoneda, monto1)
+    console.log("El costo de su operación es de" + " " + comprar + " " + "pesos Argentinos.");
+}
+
+function ventaDeMoneda(ventaMoneda,monto2) {
+    vender=operacion(ventaMoneda,monto2)
+    console.log("Usted recibirá" + " " + vender + " " + "pesos Argentinos.");
+}
+
+let indexDivisa;
+
+function divisasVenta(nombreDivisa, montoCambio) {
+    indexDivisa= divisas.indexOf(divisas.find(Moneda => Moneda.nombre === nombreDivisa)),
+    ventaDeMoneda(divisas[indexDivisa].venta,montoCambio)   
+}
+
+function divisasCompra(nombreDivisa, montoCambio) {
+    indexDivisa= divisas.indexOf(divisas.find(Moneda => Moneda.nombre === nombreDivisa)),
+    compraDeMoneda(divisas[indexDivisa].compra,montoCambio)
+}
+
+function procesarOperacion(transac,divi,cant) {
+    if (transac === "venta") {
+        divisasVenta(divi,cant)
+    } else {
+        divisasCompra(divi,cant)
+    }
+    
+}
+
+
+procesarOperacion(transaccion, divisaACambiar,montoACambiar);
